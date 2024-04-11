42d77c837e53e352ab905afd94975f53c7d9b5b0
Anthony <121622942+carlidevfr@users.noreply.github.com>
2024-04-11 14:00:27 +0200

    Update Regenerate.php

--- src/Model/Common/Regenerate.php
+++ src/Model/Common/Regenerate.php
@@ -3,14 +3,23 @@ require_once './src/Model/Common/Model.php';
 
 class Regenerate extends Model
 {
-    public function regenerateSqlProd($chemin_sql) {
+    public function regenerateSqlProd($chemin_sql)
+    {
         try {
             $pdo = $this->connexionPDO();
             $sql = file_get_contents($chemin_sql);
             $pdo->exec($sql);
             return true;
         } catch (PDOException $e) {
-            // Gérer les erreurs
+            $log = sprintf(
+                "%s %s %s %s %s",
+                date('Y-m-d- H:i:s'),
+                $e->getMessage(),
+                $e->getCode(),
+                $e->getFile(),
+                $e->getLine()
+            );
+            error_log($log . "\n\r", 3, './src/error.log');
             return false;
         }
     }
